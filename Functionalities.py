d95a0224384b5b43c52da5edf246209263da67d6
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-05-21 15:22:54 -0300

    refactor: code improvement

--- Functionalities.py
+++ Functionalities.py
@@ -6,6 +6,48 @@ import mediapipe as mp
 
 face_info = [None, None, None]
 face_detected = False
+phrases = [
+    [
+        "Are some technical objects just in the background, while the closer one gets to an epistemic situation, the more attention needs to be paid to the technical objects that are implied",
+        "Machinic creativity not as the attribute of a technical individual, but as an operation of regulation of the technical assemblage",
+        "Thinking the living being in the Technical Milieu and as a medium"
+    ],
+    [
+        "What are the particular spatial conditions that allow for epistemic phenomena to occur? Would a word such as “density” or “saturation” be appropriate to convey how those minute moments and events seem to pull the experiment together?",
+        "The aspiration to follow technical objects in their evolution is driven by an anthropological and ethical interest. Techniques have meaning and express life",
+        "To understand techniques as an expression of life should not be mistaken as an esoteric claim: Technical objects are the result of 'social' actions, since they are actions of human beings. Within the realm of the social, technical objects transform and structural human practices"
+    ],
+    [
+        "Binarisms that, mistaken for veritable states in and of themselves, can mislead our self-conceptions and lead us to believe that we are not metastable, but moving inexorably towards an a priori set point",
+        "Far from conquering nature or reshaping his environment, primitive man’s first concern was to utilize his overdeveloped, intensely active nervous system, and to give form to a human self, set apart from his original animal self by the fabrication of symbols",
+        "We and our tools have become “hybrids”—“cyborgs”"
+    ],
+    [
+        "Technology is not anthropologically universal, being enabled and constrained by particular cosmologies, which go beyond functionalities and utilities",
+        "This transfer from intuition to calculation and construction can be interpreted as ”an emancipation of human beings from their conditions earthly animals” “transcendentally”, on the other hand, such a crisis “remains to be thought through”",
+        "Whenever we construct knowledge around an unfamiliar Other, we make what is local and familiar to them familiar to us through a process of reduction by passing what is observed through our own concepts of the world, in what amounts to a “globalization” of knowledge"
+    ],
+    [
+        "We should be aware that the concepts we hold to be very familiar and “natural” can themselves be subject to other interpretations and consequently, reductions, when viewed from the perspective of a cosmological or cultural Other",
+        "Under the impression of a penetration of technical phenomena into the deeper layers of human relations of being, as cybernetic machines have made obvious, one is forced to see technology as a possible solution to that anthropologically determinable disproportion [between man and nature]. Through technology, man creates for himself an environment appropriate to his dual role as a natural and spiritual being",
+        "The cybernetic extension of modern technology means its extension under the skin of the world; Technology can no longer be considered in any way isolated (objectified) from the world process and its sociological, ideological, and vital phases. It involves everything, it has been taken on an intensified consuming character. Literature, art, music take on its features"
+    ],
+    [
+        "These socio-technological systems are less a fact, however, than an invention through which digital cultures, deeply rooted in technological forms of cooperation, can come into existence",
+        "Not that comparisons between the animal and human worlds are anything particularly novel; what is new is the fact that today they are meant seriously, that the anthropological supremacy of the human is no longer capable of upholding itself in the current technical-organic overall structure.",
+        "Such a rewriting of art history and sound art practices will have to be acutely aware of issues of globalization, postcolonialism, and decolonization; it will need to integrate representations on the Internet with those of live concerts, installations, and interviews; it must reach out toward anthropological methods; and it should make aesthetic considerations central to its analysis"
+    ],
+    [
+        "Where there is presence and affect, there remains abundance, excess and the possibility of recording",
+        "The “technique” of an action refers to the means employed as opposed to the meaning or end to which the action is, in the last analysis, oriented",
+        "“Rational” technique is a choice of means which is consciously and systematically oriented to the experience and reflection of the actor, which consists, at the highest level of rationality, in scientific knowledge"
+    ],
+    [
+        "In such a negative anthropological situation, belief means the ability to project possibilities for bifurcation, in a system that is on the way to becoming closed and requires a change that by itself the system cannot calculate. Such possibilities are those prescribed and certified by work-knowledge, life-knowledge, and conceptual knowledge, that is, by knowledge of how to live, do, and think",
+        "Some anthropological data suggest that the concept of 'person' is not limited to the human class, but that animals or objects may possess 'person' qualities in that they are perceived as origins and causes of changes in the field, as endowed with a power of their own",
+        "Relation must be understood as something that precedes the formation of the terms of the relation (subject, object, individual, groups, indeed all forms of collective human and nonhuman agents): predominantly, it must traverse all modes and levels of Being, from the micro to the macro, meaning that it is a 'modality of being'"
+    ],
+]
 
 # "constantes"
 SMALL_DISTANCE = 120
@@ -146,84 +188,6 @@ def get_positions(face_landmarks, image):
 # TODO: phrases é um vetor que ocupa muito espaço, armazenar de uma maneira diferente.
 def get_text_and_color(distance, i, marca_prox, marca_dist):
     colors = [(0, 255, 0), (0, 255, 255), (0, 0, 255)]  # Verde, Amarelo, Vermelho
-    phrases = [
-        ["Ola, estou aqui para ajudar!", "Registro: Pessoa comum", "Intruso detectado: A maquina observa."],
-        ["Tenha um otimo dia!", "Registro: Sujeito nao identificado", "Nas sombras ciberneticas, estamos te vigiando."],
-        ["Voce e muito especial para nos.", "Registro: Visitante desconhecido", "A tecnologia nunca fecha os olhos."],
-        ["Sinto muito, nao posso fazer isso.", "Registro: Entidade digital", "Seus movimentos sao agora digitais."],
-        ["Estamos aqui para apoia-lo.", "Registro: Ser humano autenticado", "Cameras roboticas te seguem como sombras implacaveis."],
-        ["Por favor, forneça mais informacoes.", "Registro: Identidade nao verificada", "Voce e agora parte de nossa opera digital."],
-        ["Tudo ficara bem.", "Registro: Presenca detectada", "Seu rosto e apenas um pixel na tela do desconhecido."],
-        ["Voce e valioso para nos.", "Registro: Visitante nao autorizado", "As sombras do ciberespaco sussurram."],
-        ["Obrigado por escolher nossos servicos.", "Registro: Usuario comum", "O DNA digital da humanidade se desenrola."],
-        ["Nao consigo responder a essa pergunta.", "Registro: Consulta anonima", "Aqui, somos todos prisioneiros da Matrix."],
-        ["Voce e unico de sua maneira.", "Registro: Entidade desconhecida", "Nossas vidas sao tracadas na areia do tempo."],
-        ["Estou aqui para ajudar.", "Registro: Identidade nao verificada", "O olho eletronico nao pode ser enganado."],
-        ["Como voce esta hoje?", "Registro: Pessoa sem identificacao", "Sua presenca digital e uma linha de codigo."],
-        ["Vamos encontrar uma solucao.", "Registro: Ser humano autenticado", "As sombras ciberneticas escondem segredos."],
-        ["Voce e especial para nos.", "Registro: Visitante nao autorizado", "Cada clique e um passo mais fundo na toca do coelho."],
-        ["Sinta-se a vontade para perguntar.", "Registro: Identidade desconhecida", "As redes invisiveis tecem nosso destino digital."],
-        ["Desculpe, nao compreendi.", "Registro: Consulta nao identificada", "A presenca humana e apenas um traco de pixels."],
-        ["Estamos aqui para ajudar.", "Registro: Entidade digital", "A memoria da maquina nunca se desvanece com o tempo."],
-        ["Como posso te auxiliar?", "Registro: Pessoa comum", "Cada acao e uma gota no oceano da ciber-realidade."],
-        ["O que voce gostaria de fazer?", "Registro: Identidade nao verificada", "Nossa existencia e uma linha de codigo em constante evolucao."],
-        ["Vamos resolver isso juntos.", "Registro: Ser humano autenticado", "Aqui, a privacidade e uma ilusao fugaz e efemera."],
-        ["Sua opiniao e importante para nos.", "Registro: Visitante desconhecido", "Os observadores ciberneticos veem o invisivel."],
-        ["Estamos prontos para ajudar.", "Registro: Identidade nao verificada", "Somos marionetes nas maos do mestre da tecnologia."],
-        ["Como posso ser util?", "Registro: Pessoa sem identificacao", "O registro digital e o testemunho de nossa jornada."],
-        ["Ficarei feliz em te auxiliar.", "Registro: Visitante nao autorizado", "A vigilancia e o preco que pagamos pela conveniencia."],
-        ["Em que posso te ajudar hoje?", "Registro: Consulta anonima", "O olho da maquina e onipresente e onisciente."],
-        ["Voce e importante para nos.", "Registro: Identidade nao verificada", "As redes invisiveis tecem nosso destino digital."],
-        ["Como posso te apoiar?", "Registro: Ser humano autenticado", "Nossas vidas sao tracadas na areia do tempo cibernetico."],
-        ["Sinta-se a vontade para perguntar.", "Registro: Pessoa sem identificacao", "A memoria da maquina e eterna e intransigente."],
-        ["Estou a disposicao.", "Registro: Identidade desconhecida", "Os segredos digitais estao escondidos nas entrelinhas."],
-        ["O que voce precisa?", "Registro: Consulta nao identificada", "Cada acao e uma gota no oceano da ciber-realidade."],
-        ["Vamos encontrar uma solucao juntos.", "Registro: Visitante desconhecido", "Nossa existencia e uma linha de codigo em constante evolucao."],
-        ["Voce e unico.", "Registro: Entidade digital", "Aqui, a privacidade e uma ilusao fugaz e efemera."],
-        ["O que posso fazer por voce?", "Registro: Identidade nao verificada", "Os observadores ciberneticos veem o invisivel."],
-        ["Como posso ser de auxilio?", "Registro: Pessoa comum", "Somos marionetes nas maos do mestre da tecnologia."],
-        ["Estou aqui para te ajudar.", "Registro: Ser humano autenticado", "O registro digital e o testemunho de nossa jornada."],
-        ["Como posso ser util hoje?", "Registro: Visitante nao autorizado", "A vigilancia e o preco que pagamos pela conveniencia."],
-        ["Em que posso te auxiliar hoje?", "Registro: Consulta anonima", "O olho da maquina e onipresente e onisciente."],
-        ["Voce e valioso para nos.", "Registro: Identidade nao verificada", "As redes invisiveis tecem nosso destino digital."],
-        ["Como posso te apoiar?", "Registro: Ser humano autenticado", "Nossas vidas sao tracadas na areia do tempo cibernetico."],
-        ["Sinta-se a vontade para perguntar.", "Registro: Pessoa sem identificacao", "A memoria da maquina e eterna e intransigente."],
-        ["Estou a disposicao.", "Registro: Identidade desconhecida", "Os segredos digitais estao escondidos nas entrelinhas."],
-        ["O que voce precisa?", "Registro: Consulta nao identificada", "Cada acao e uma gota no oceano da ciber-realidade."],
-        ["Vamos encontrar uma solucao juntos.", "Registro: Visitante desconhecido", "Nossa existencia e uma linha de codigo em constante evolucao."],
-        ["Voce e unico.", "Registro: Entidade digital", "Aqui, a privacidade e uma ilusao fugaz e efemera."],
-        ["O que posso fazer por voce?", "Registro: Identidade nao verificada", "Os observadores ciberneticos veem o invisivel."],
-        ["Como posso ser de auxilio?", "Registro: Pessoa comum", "Somos marionetes nas maos do mestre da tecnologia."],
-        ["Estou aqui para te ajudar.", "Registro: Ser humano autenticado", "O registro digital e o testemunho de nossa jornada."],
-        ["Como posso ser util hoje?", "Registro: Visitante nao autorizado", "A vigilancia e o preco que pagamos pela conveniencia."],
-        ["Em que posso te auxiliar hoje?", "Registro: Consulta anonima", "O olho da maquina e onipresente e onisciente."],
-        ["Voce e valioso para nos.", "Registro: Identidade nao verificada", "As redes invisiveis tecem nosso destino digital."],
-        ["Como posso te apoiar?", "Registro: Ser humano autenticado", "Nossas vidas sao tracadas na areia do tempo cibernetico."],
-        ["Sinta-se a vontade para perguntar.", "Registro: Pessoa sem identificacao", "A memoria da maquina e eterna e intransigente."],
-        ["Estou a disposicao.", "Registro: Identidade desconhecida", "Os segredos digitais estao escondidos nas entrelinhas."],
-        ["O que voce precisa?", "Registro: Consulta nao identificada", "Cada acao e uma gota no oceano da ciber-realidade."],
-        ["Vamos encontrar uma solucao juntos.", "Registro: Visitante desconhecido", "Nossa existencia e uma linha de codigo em constante evolucao."],
-        ["Voce e unico.", "Registro: Entidade digital", "Aqui, a privacidade e uma ilusao fugaz e efemera."],
-        ["O que posso fazer por voce?", "Registro: Identidade nao verificada", "Os observadores ciberneticos veem o invisivel."],
-        ["Como posso ser de auxilio?", "Registro: Pessoa comum", "Somos marionetes nas maos do mestre da tecnologia."],
-        ["Estou aqui para te ajudar.", "Registro: Ser humano autenticado", "O registro digital e o testemunho de nossa jornada."],
-        ["Como posso ser util hoje?", "Registro: Visitante nao autorizado", "A vigilancia e o preco que pagamos pela conveniencia."],
-        ["Em que posso te auxiliar hoje?", "Registro: Consulta anonima", "O olho da maquina e onipresente e onisciente."],
-        ["Voce e valioso para nos.", "Registro: Identidade nao verificada", "As redes invisiveis tecem nosso destino digital."],
-        ["Como posso te apoiar?", "Registro: Ser humano autenticado", "Nossas vidas sao tracadas na areia do tempo cibernetico."],
-        ["Sinta-se a vontade para perguntar.", "Registro: Pessoa sem identificacao", "A memoria da maquina e eterna e intransigente."],
-        ["Estou a disposicao.", "Registro: Identidade desconhecida", "Os segredos digitais estao escondidos nas entrelinhas."],
-        ["O que voce precisa?", "Registro: Consulta nao identificada", "Cada acao e uma gota no oceano da ciber-realidade."],
-        ["Vamos encontrar uma solucao juntos.", "Registro: Visitante desconhecido", "Nossa existencia e uma linha de codigo em constante evolucao."],
-        ["Voce e unico.", "Registro: Entidade digital", "Aqui, a privacidade e uma ilusao fugaz e efemera."],
-        ["O que posso fazer por voce?", "Registro: Identidade nao verificada", "Os observadores ciberneticos veem o invisivel."],
-        ["Como posso ser de auxilio?", "Registro: Pessoa comum", "Somos marionetes nas maos do mestre da tecnologia."],
-        ["Estou aqui para te ajudar.", "Registro: Ser humano autenticado", "O registro digital e o testemunho de nossa jornada."],
-        ["Como posso ser util hoje?", "Registro: Visitante nao autorizado", "A vigilancia e o preco que pagamos pela conveniencia."],
-        ["Em que posso te auxiliar hoje?", "Registro: Consulta anonima", "O olho da maquina e onipresente e onisciente."],
-        ["Voce e valioso para nos.", "Registro: Identidade nao verificada", "As redes invisiveis tecem nosso destino digital."],
-        ["Como posso te apoiar?", "Registro: Ser humano autenticado", "Nossas vidas sao tracadas na areia do tempo cibernetico."]
-    ]
 
     if distance > marca_prox:
         frase = phrases[i][0] # ...[0] indica a versão amigável da frase
